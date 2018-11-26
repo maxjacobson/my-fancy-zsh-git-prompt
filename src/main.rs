ae83acf5c2961fd9dd70dac41f75aa072f84407f
Max Jacobson <max@hardscrabble.net>
2018-11-25 21:23:45 -0500

    Add first cut of my-fancy-zsh-git-prompt
    This re-implements a mediocre shell script as a mediocre rust script.
    
    It adds one feature which I struggled to implement in shell, which is to
    display the current working directory relative to the current git
    repository when those two directories are not the same.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,309 @@
+extern crate git2;
+
+use std::env::current_dir;
+use std::path::PathBuf;
+
+use git2::{DiffOptions, Repository, RepositoryState};
+
+struct ZshOutput {
+    is_bold: bool,
+    color: Option<String>,
+    text: String,
+}
+
+impl ZshOutput {
+    fn new(text: &str) -> Self {
+        ZshOutput {
+            text: text.to_string(),
+            is_bold: false,
+            color: None,
+        }
+    }
+
+    fn set_color(&mut self, color: &str) {
+        self.color = Some(color.to_string());
+    }
+
+    fn make_bold(&mut self) {
+        self.is_bold = true;
+    }
+
+    fn output(&self) -> String {
+        let mut result = String::new();
+
+        if self.is_bold {
+            result.push_str("%B");
+        }
+
+        match self.color {
+            Some(ref c) => {
+                result.push_str("%F{");
+                result.push_str(&format!("{}", c));
+                result.push_str("%}");
+            }
+            None => {}
+        }
+
+        result.push_str(&self.text);
+
+        match self.color {
+            Some(ref _c) => result.push_str("%f"),
+            None => {}
+        }
+
+        if self.is_bold {
+            result.push_str("%b");
+        }
+
+        result
+    }
+}
+
+struct DirectoryContext {
+    path: PathBuf,
+    repository: Option<Repository>,
+}
+
+impl DirectoryContext {
+    fn current_directory_short_name(&self) -> Option<String> {
+        self.directory_short_name(&self.path)
+    }
+
+    fn directory_short_name(&self, path: &PathBuf) -> Option<String> {
+        if path.is_dir() {
+            path.file_name()
+                .map(|name_os_str| name_os_str.to_str().map(|name| name.to_string()))
+                .unwrap_or(None)
+        } else {
+            None
+        }
+    }
+
+    fn format_subdirectory_path(
+        &self,
+        repository_path: Option<&std::path::Path>,
+        current_working_directory: &PathBuf,
+    ) -> Option<String> {
+        match repository_path {
+            Some(repository_path) => {
+                let repository_path_buf = repository_path.to_path_buf();
+
+                match self.directory_short_name(&repository_path_buf) {
+                    Some(short_name) => {
+                        let mut result = String::new();
+                        result.push_str(&short_name);
+                        result.push_str("/");
+
+                        let diff = current_working_directory.strip_prefix(repository_path_buf);
+
+                        match diff {
+                            Ok(diff_path) => match diff_path.to_str() {
+                                Some(diff_path_str) => result.push_str(diff_path_str),
+                                None => {}
+                            },
+                            Err(_) => {}
+                        }
+
+                        Some(result)
+                    }
+                    None => None,
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn path_summary(&self) -> Option<String> {
+        match self.repository {
+            Some(ref repository) => {
+                let repository_workdir = repository.workdir();
+                if self.paths_match(repository_workdir, &self.path) {
+                    self.current_directory_short_name()
+                } else {
+                    self.format_subdirectory_path(repository_workdir, &self.path)
+                }
+            }
+            None => self.current_directory_short_name(),
+        }
+    }
+
+    fn paths_match(
+        &self,
+        repository_path: Option<&std::path::Path>,
+        current_working_directory: &PathBuf,
+    ) -> bool {
+        match repository_path {
+            Some(repository_path) => repository_path == current_working_directory,
+            None => false,
+        }
+    }
+}
+
+impl std::fmt::Display for DirectoryContext {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match self.path_summary() {
+            Some(name) => write!(f, "{}", name),
+            None => Ok(()),
+        }
+    }
+}
+
+impl std::fmt::Display for ZshOutput {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "{}", self.output())
+    }
+}
+
+fn summarize(repository: &Repository) -> ZshOutput {
+    match repository.state() {
+        RepositoryState::Clean => match repository.head() {
+            Ok(head_reference) => match head_reference.target() {
+                Some(oid) => {
+                    let commit = repository.find_commit(oid).unwrap();
+
+                    let head_tree = commit.tree().unwrap();
+
+                    let mut diff_options = DiffOptions::new();
+                    diff_options.include_untracked(true);
+
+                    let files_changed = repository
+                        .diff_tree_to_workdir(Some(&head_tree), Some(&mut diff_options))
+                        .unwrap()
+                        .stats()
+                        .unwrap()
+                        .files_changed();
+
+                    let mut is_branch = true;
+
+                    let branch_name = if head_reference.is_branch() {
+                        head_reference
+                            .shorthand()
+                            .unwrap_or_else(|| "(unknown branch)")
+                            .to_string()
+                    } else {
+                        is_branch = false;
+                        format!("{}", head_reference.target().unwrap())
+                    };
+
+                    if files_changed > 0 {
+                        let mut output = ZshOutput::new(&format!("{}*", &branch_name));
+                        output.set_color("red");
+                        output
+                    } else {
+                        let mut output = ZshOutput::new(&branch_name);
+                        if is_branch {
+                            output.set_color("green");
+                        } else {
+                            output.set_color("blue");
+                        }
+                        output
+                    }
+                }
+                None => {
+                    let mut output = ZshOutput::new("(???)");
+                    output.set_color("red");
+                    output
+                }
+            },
+            Err(_) => {
+                let mut output = ZshOutput::new("(no commits yet)");
+                output.set_color("yellow");
+                output
+            }
+        },
+        RepositoryState::Merge => {
+            let mut output = ZshOutput::new("(merging)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::Revert => {
+            let mut output = ZshOutput::new("(reverting)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::RevertSequence => {
+            let mut output = ZshOutput::new("(reverting)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::CherryPick => {
+            let mut output = ZshOutput::new("(cherry-picking)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::CherryPickSequence => {
+            let mut output = ZshOutput::new("(cherry-picking)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::Bisect => {
+            let mut output = ZshOutput::new("(bisecting)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::Rebase => {
+            let mut output = ZshOutput::new("(rebasing)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::RebaseInteractive => {
+            let mut output = ZshOutput::new("(rebasing)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::RebaseMerge => {
+            let mut output = ZshOutput::new("(rebasing)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::ApplyMailbox => {
+            let mut output = ZshOutput::new("(mailbox-applying)");
+            output.set_color("magenta");
+            output
+        }
+        RepositoryState::ApplyMailboxOrRebase => {
+            let mut output = ZshOutput::new("(mailbox-applying)");
+            output.set_color("magenta");
+            output
+        }
+    }
+}
+
+fn print_details(dir: DirectoryContext) {
+    match dir.repository {
+        Some(ref repository) => {
+            println!("{} {} ", dir, summarize(repository));
+        }
+        None => {
+            let mut output = ZshOutput::new("(not repo)");
+            output.set_color("blue");
+            output.make_bold();
+            println!("{} {} ", dir, output);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dir = current_dir();
+    if dir.is_err() {
+        return;
+    }
+
+    let dir_path = dir.unwrap();
+    let mut dir_context = DirectoryContext {
+        path: dir_path.clone(),
+        repository: None,
+    };
+
+    let repository = match Repository::discover(&dir_path) {
+        Ok(r) => r,
+        Err(_) => {
+            print_details(dir_context);
+            return;
+        }
+    };
+
+    dir_context.repository = Some(repository);
+
+    print_details(dir_context);
 }
